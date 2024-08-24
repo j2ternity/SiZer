7d5d1542a87cf6a42978787dada94df7bcc25496
j2ternity <55611413+j2ternity@users.noreply.github.com>
2024-08-25 03:03:25 +0900

    Add files via upload

--- figure_example.R
+++ figure_example.R
@@ -63,6 +63,68 @@ color_adj <- adjustcolor(color, alpha=0.25)
 surf3D(matrix(grid_pt_x, nrow=grid, byrow=T), matrix(grid_pt_y, nrow=grid, byrow=T), matrix(kde, nrow=grid, byrow=T), 
        colvar = matrix(color_adj, nrow=grid, byrow=T), colkey = TRUE, box = TRUE, bty = "b", phi = 40, theta = -40)
 
+
+############################################
+##########Movie 3###########################
+############################################
+
+library(MASS)
+library(tidyverse) 
+library(circular)
+library(bio3d)
+
+pdb <- read.pdb("9ENJ") 
+a <- torsion.pdb(pdb)
+
+data <- cbind(a$phi/180*pi,a$psi/180*pi)
+data <- data[-which(is.na(data[,1])|is.na(data[,2])),]
+
+x1 <- data[, 1]+2*pi*(sign(data[, 1])==-1)
+x2 <- data[, 2]+2*pi*(sign(data[, 2])==-1)
+
+plot(x1, x2, xlim=c(0, 2*pi), ylim=c(0, 2*pi), xaxt='n', yaxt='n', xlab = expression(phi), ylab = expression(psi), pch=16)
+axis(1, at=seq(0, 2*pi, length.out=5), labels=c("0", expression(pi/2), expression(pi), expression(3*pi/2), expression(2*pi)))
+axis(2, at=seq(0, 2*pi, length.out=5), labels=c("0", expression(pi/2), expression(pi), expression(3*pi/2), expression(2*pi)))
+
+grid <- 24
+seq_kappa <- 10^seq(0, 2, length=200)
+res <- torus_sizer(x1, x2, grid, seq_kappa, B=10000, alpha=0.05, method=2)
+
+for(i in 1:length(res)){
+  grid_pt <- res[[i]]$grid_pt
+  arrow <- res[[i]]$arrow
+  ESS <- res[[i]]$ESS
+  signif <- res[[i]]$signif
+  color <- res[[i]]$color
+  
+  png(paste("Rplot_", i, ".png"), width=800, height=800) #If you want to save as files, run this line along with other line
+  par(mar = c(7, 4, 2, 2))
+  
+  plot(NULL, xlim=c(0, 2*pi), ylim=c(0, 2*pi), xaxt='n', yaxt='n', xlab = expression(phi), ylab = expression(psi))
+  axis(1, at=seq(0, 2*pi, length.out=5), labels=c("0", expression(pi/2), expression(pi), expression(3*pi/2), expression(2*pi)))
+  axis(2, at=seq(0, 2*pi, length.out=5), labels=c("0", expression(pi/2), expression(pi), expression(3*pi/2), expression(2*pi)))
+  
+  idx <- 1
+  for(k in 0:(grid-1)){
+    for(j in 0:(grid-1)){
+      polygon(c(2*j-1, 2*j-1, 2*j+1, 2*j+1)*pi/grid, c(2*k-1, 2*k+1, 2*k+1, 2*k-1)*pi/grid, col=color[idx], density=NA)
+      idx <- idx+1
+    }
+  }
+  
+  for (j in 1:length(ESS)){
+    if(ESS[j]>=5){
+      if(signif[j]==TRUE){
+        arrows(grid_pt[1, j], grid_pt[2, j], grid_pt[1, j]+arrow[1, j], grid_pt[2, j]+arrow[2, j], length = 0.04, lwd=2)
+      }else{
+        points(grid_pt[1, j], grid_pt[2, j], pch=20)
+      }
+    }
+  }
+  mtext(paste(expression(kappa), "=", toString(round(seq_kappa[i], digits=3), width=11)), side = 1, line = -2.5, cex = 1.4, outer = TRUE)
+  dev.off() #If you want to save as files, run this line along with other line
+}
+
 ############################################
 ##########Figure 4 & Movie 4-5##############
 ############################################
